e5c7e824d28947306a736469bd33fd6a232ee9d8
Elizabeth Hills <e.a.hills13@gmail.com>
2013-05-01 19:32:19 -0700

    Up-to-date - Week 4

--- Assets/Editor/EditorWindow.cs
+++ Assets/Editor/EditorWindow.cs
@@ -8,8 +8,19 @@ public class MyEditor : EditorWindow
 	bool myBool = true;
 	float myFloat = 1.23f;
 
-	// Add menu item named "My Window" to the Window menu
-	[MenuItem("Window/My Window")]
+	// Add menu item named "My Editor" to the Window menu
+	[MenuItem("Tools/ Set Mass")]
+	static void SetMass()
+	{
+		Debug.Log ("Setting Mass");
+	}
+	
+	static void Init()
+	{
+		// Get existing open window or if none, make a new one:
+		MyEditor editor = (MyEditor)EditorWindow.GetWindow(typeof (MyEditor));
+	}
+	
 	public static void ShowWindow()
 	{
 		//Show existing window instance. If one doesn't exist, make one.
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -11,8 +11,8 @@ public class PlayerMovement : MonoBehaviour
 	public Vector3 movement;
 	public Vector3 jump;
 	
-	public bool grounded = true;
-	public bool isTrigger = true;
+	public bool grounded = false;
+	public bool isTrigger = false;
 
 	// Use this for initialization
 	void Start () 
@@ -23,71 +23,124 @@ public class PlayerMovement : MonoBehaviour
 	// Update is called once per frame 
 	void Update ()
     {              
-		movement.x = horizontalMovementSpeed * Input.GetAxis("Horizontal");                
-        movement.z = 0;
+		move();
+		climb();
+		ladderInteraction();
 		
+		characterController.Move (movement * Time.deltaTime);
+	}  
+	
+	// Starting the trigger
+	public void OnTriggerEnter(Collider c)
+	{
+		if (c.gameObject.tag == "Ladder")
+		{
+			isTrigger = true;
+		}
+	}
+	
+	/*
+	// Being idle trigger
+	public void OnTriggerIdle(Collider c)
+	{
+		if (c.gameObject.tag == "Ladder")
+		{
+			isTrigger = true;
+		}
+	}
+	*/
+	
+	// Exiting the trigger
+	public void OnTriggerExit(Collider c)
+	{
+		if (c.gameObject.tag == "Ladder")
+		{
+			isTrigger = false;
+		}
+	}
+	
+	public void move()
+	{
+		movement.x = horizontalMovementSpeed * Input.GetAxis("Horizontal");                
+        movement.z = 0;	 	
+	}
+		
+	// Jumping function
+	/*
+	public void OnJump()
+	{
+		if (grounded == false & Input.GetKeyDown("space"))
+		{
+			movement.y = 10f;
+
+		    //yield return WaitForSeconds(2f);
+					
+		    Debug.Log ("Jumping on/off the ladder");
+		}
+	}
+	*/
+	
+	public void climb()
+	{
 		// Used for jumping
 		if (grounded == true & isTrigger == true)
 		{
 			if (grounded == true & Input.GetKeyDown("space"))
 			{
-				grounded = false;
-				movement.y = 10f;
+				//grounded = false;
+				movement.y -= 10f;
 
 				//yield return WaitForSeconds(2f);
-				
-				// Debug.Log("space was pressed");	
+					
+				Debug.Log ("Jumping on/off the ladder");
 			}
 		}
 		// Used for coming back down
-		else if (grounded == false)
+		else
 		{
-				movement.y -= gravity * Time.deltaTime;
-				if (movement.y <= 0)
-				{
-					grounded = true;
-				    movement.y = -5f;
-				}
-		}
-		
-		
+			movement.y -= gravity * Time.deltaTime;
+			if (movement.y <= 0)
+			{
+				grounded = false;
+			    movement.y -= 5f;
+			}
+		}	
+	}
+	
+	public void ladderInteraction()
+	{
 		// Ladder interaction
 		if (isTrigger)
 		{
 			// Climbing up
 			if (Input.GetAxis ("Vertical") > 0)	
 			{
-				movement.y = 1f;
+				movement.y -= 1f;
 				
-				transform.Translate (0, 1 * Time.deltaTime, 0);
-				
-				//Debug.Log ("climbing");
-				
-				// !!!!! Need to have a single call !!!!!
-				if (grounded == false & Input.GetKeyDown("space"))
+				if (grounded == true & Input.GetKeyDown("space"))
 				{
-					movement.y = 10f;
+					movement.y -= 10f;
 
 					//yield return WaitForSeconds(2f);
 					
-					//Debug.Log ("Jumping on/off the ladder");
+					Debug.Log ("Jumping on/off the ladder");
 				}
 			}
 			// Climbing down
-			else if (Input.GetAxis ("Vertical") < 0)
+			else if (Input.GetAxis ("Vertical") <= 0)
 			{
-				movement.y = 2f;
+				movement.y -= 2f;
 				
-				transform.Translate (0, -1 * Time.deltaTime, 0);
+				//OnJump ();
 				
-				// !!!!! Need to have a single call !!!!!
 				if (grounded == false & Input.GetKeyDown("space"))
 				{
-					movement.y = 10f;
+					//grounded = false;
+					movement.y -= 10f;
 
 					//yield return WaitForSeconds(2f);
 					
-					//Debug.Log ("Jumping on/off the ladder");
+					Debug.Log ("Jumping on/off the ladder");
 				}
 			}
 			
@@ -114,35 +167,6 @@ public class PlayerMovement : MonoBehaviour
 				}
 			}
 			*/
-		}
-	
-		characterController.Move (movement * Time.deltaTime); 
-	}  
-	
-	// Starting the trigger
-	public void OnTriggerEnter(Collider c)
-	{
-		if (c.gameObject.tag == "Ladder")
-		{
-			isTrigger = true;
-		}
-	}
-	
-	// Being idle trigger
-	public void OnTriggerIdle(Collider c)
-	{
-		if (c.gameObject.tag == "Ladder")
-		{
-			isTrigger = true;
-		}
-	}
-	
-	// Exiting the trigger
-	public void OnTriggerExit(Collider c)
-	{
-		if (c.gameObject.tag == "Ladder")
-		{
-			isTrigger = false;
 		}
 	}
 }
